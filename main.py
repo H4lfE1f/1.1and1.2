8224d32f4816e8958aa4794ae40e445e156917d4
mirea <mirea@rd.mirea.ru>
2021-02-16 18:42:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+# var_int = 5
+# var_int1 = 6
+# var_str = "hello world"
+# result = "Число 1: {}\nЧисло 2:{}\nСлова:{}"\
+#     .format(var_int, var_int1, var_str)
+# print (result)
+#
+# x = int (input("введите число: "))
+# print (x)
+# x = int (input("введите число: "))
+# seconds = x
+# minutes = x // 60
+# hours = minutes // 60
+#
+# print ("%02d:%02d:%02d" % (hours, minutes % 60, seconds % 60))
+# print ("%02d:%02d" % (minutes, seconds % 60))
+# n = int (input("введите число n: "))
+# temp = str (n)
+# t1 = temp + temp
+# t2 = temp + temp + temp
+# comp = n + int(t1) + int(t2)
+# print (f"результат равен: {comp}")
+# x = int (input("Введите число:"))
+# r = -1
+# while x > 10:
+#     d = x % 10
+#     x //= 10
+#     if d > r:
+#         r = d
+# print (r)
+# x = int (input("Введите выручку:"))
+# y = int (input("Введите издержку:"))
+# if x > y:
+#     c = x - y
+#     print(f"Прибыль: {c}")
+#     print (f"Рентабельность выручки: {c/x}")
+#     n = int(input("Введите кол-во сотрудников :"))
+#     print(f"Прибыль на сотрудника составила: {c / n}")
+#
+# elif x < y:
+#     print ("Убыток")
+# n = int (input("Введите кол-во сотрудников :"))
+# x = int(input("Введите начальную цель в км: "))
+# y = int(input("Введите искомую цель в км : "))
+# day = 1
+# while y-x > 0:
+#     x = x + (x * 0.1)
+#     day += 1
+#     print (day)
+
+
